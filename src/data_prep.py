2cf07eb11ff74db845d7e33945e81548f48713ff
Jitheshn <jitheshnairp@hotmail.com>
2025-08-04 08:46:58 +0530

    Initial repo setup and data versioning

---
+++ src/data_prep.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+
+def load_and_save_data():
+    housing = fetch_california_housing(as_frame=True)
+    df = housing.frame
+    df.to_csv("data/raw/housing.csv", index=False)
+
+if __name__ == "__main__":
+    load_and_save_data()
