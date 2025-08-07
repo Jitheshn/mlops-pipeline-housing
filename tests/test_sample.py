57bfd42f5813a2729a041bb765ea1b4f91bc1d07
Jitheshn <jitheshnairp@hotmail.com>
2025-08-07 05:48:00 +0530

    Add CI/CD pipeline with DockerHub push

---
+++ tests/test_sample.py
@@ -0,0 +1,27 @@
+# tests/test_sample.py
+
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to the California Housing Price Prediction API"}
+
+def test_prediction():
+    sample_input = {
+        "MedInc": 8.3252,
+        "HouseAge": 41.0,
+        "AveRooms": 6.984127,
+        "AveBedrms": 1.02381,
+        "Population": 322.0,
+        "AveOccup": 2.555556,
+        "Latitude": 37.88,
+        "Longitude": -122.23
+    }
+    response = client.post("/predict", json=sample_input)
+    assert response.status_code == 200
+    assert "prediction" in response.json()
+    assert isinstance(response.json()["prediction"], float)
